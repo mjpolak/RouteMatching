91565ae8b43ff0a817dfd35b5f1a56e5551a6d33
mjpolak <marcin.jakub.polak@gmail.com>
2019-10-13 21:24:20 +0200

    logging added

---
+++ Utils/LoggingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace RouteMatching.Utils
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly Serilog.ILogger log;
+
+        public LoggingMiddleware(RequestDelegate next, Serilog.ILogger log)
+        {
+            _next = next;
+            this.log = log;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+            
+            log.Information("Request, Path: {Path}, Method: {Method} Headers: {@Headers}", context.Request.Path, context.Request.Method,context.Request.Headers);
+
+            await _next(context);
+
+            log.Information("Response for request Path: {Path}, Method: {Method} Headers: {@Headers}", context.Request.Path, context.Request.Method, context.Request.Headers);
+            log.Information("Respone Status code:{StatusCode}", context.Response.StatusCode);
+
+        }
+    }
+
+    public static class LoggingMiddlewareMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLogging(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoggingMiddleware>();
+        }
+    }
+}
