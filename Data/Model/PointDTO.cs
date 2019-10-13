b961875a1d32c000d753dc23afd9c4485ab54897
mjpolak <marcin.jakub.polak@gmail.com>
2019-10-13 20:43:11 +0200

    backbone and repository

---
+++ Data/Model/PointDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouteMatching.Data.Model
+{
+    public class PointDTO
+    {
+        public int unitId { get; set; }
+        public DateTime timedate { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public bool ignition { get; set; }
+        public bool engine { get; set; }
+        public double speed { get; set; }
+        public bool positionError { get; set; }
+        public double rpm { get; set; }
+        public double direction { get; set; }
+        public double distance { get; set; }
+    }
+}
---
+++ Services/IFleetTelemetricsService.cs
@@ -0,0 +1,15 @@
+﻿using RouteMatching.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouteMatching.Services
+{
+    public interface IFleetTelemetricsService
+    {
+        IEnumerable<RouteHeaderDTO> GetRoutesMetaData();
+
+        RouteDataDTO GetRouteDetails(string Id);
+    }
+}
